18d2bb2e619fafdad509e78e53fc43559ac5f536
Arthur buck <arthurbuck@arthurs-mbp.lan>
2023-06-14 19:13:22 -0500

    add structure to go through the table and retrieve meta data

--- secoda_project/views.py
+++ secoda_project/views.py
@@ -7,15 +7,40 @@ def table_metadata_endpoint(request):
     if request.method == 'POST':
         #got format from sqlalchemy website
         #expected format: dialect+driver://username:password@host:port/database
+
         data_base_login = request.POST.get('data_base_login')
 
-        #lets connect now!
-        print(data_base_login)
+        #assumptions: I will only ever be given one DB to go through
+        #DB could have multiple table or no tables
+        #if no tables then simply return an empty list to represent an empty table
+        #in prod I would talk to my PM about expected output in case of empty table but we are just keeping it simple to save time right now
+        # each table could have many or no rows or columns
         engine = create_engine(data_base_login)
         inspector = inspect(engine)
         #lets grab some tables to see verify we are connecting and able to read from the DB
         tables = inspector.get_table_names()
-        print(tables)
-        #looks good!
+        
+        #lets build out how I will go through the DB's
+        # TableMetadata = {
+        # columns: List[ColumnMetadata]
+        # num_rows: int
+        # schema: str
+        # database: str
+        #}
 
+        table_metadata = []
+
+        #go through the tables
+        for table in tables:
+            column_metadata = []
+            # ColumnMetadata = {
+                # col_name: str
+                # col_type: str
+                # }
+            #go through the columns
+            for column in columns:
+                
+
+
+        #will get to the Json response later
         return HttpResponse('helloWorld', 200)
